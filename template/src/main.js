e2040f8658369898205d8060039c926d4be86c29
good-templates <446100020@qq.com>
2018-05-07 13:25:51 +0800

    添加report错误日志上报,viewport-fit

--- template/src/main.js
+++ template/src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable*/
 {{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
@@ -22,7 +23,25 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+
+function formatComponentName(vm) {
+  if (vm.$root === vm) return 'root';
+
+  const name = vm._isVue ? (vm.$options && vm.$options.name) || (vm.$options && vm.$options._componentTag) : vm.name;
+  return (name ? `component <${name}>` : 'anonymous component') + (vm._isVue && vm.$options && vm.$options.__file ? ` at ${vm.$options && vm.$options.__file}` : '');
+}
+
+Vue.config.errorHandler = function (err, vm, info) {
+  const componentName = formatComponentName(vm);
+  const propsData = vm.$options && vm.$options.propsData;
+  BJ_REPORT.report({
+    msg:err,
+    target:componentName,
+    propsData:propsData,
+    info:info,
+  });
+};
+
 new Vue({
   el: '#app',
   {{#router}}
